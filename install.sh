4b3b3d79a915f3e6ceea789369b02af66e7d0d32
vtdung123 <dung123.vv@gmail.com>
2023-08-30 21:10:01 +0700

    first commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo apt-get install ros-noetic-ecl-exceptions
+sudo apt-get install ros-noetic-ecl-threads
+sudo apt-get install ros-noetic-ecl-geometry
+sudo apt-get install ros-noetic-ecl-console
+sudo apt-get install ros-noetic-ecl-mobile-robot
+sudo apt-get install ros-noetic-ecl-devices
+sudo apt-get install ros-noetic-ecl-sigslots
+sudo apt-get install ros-noetic-ecl-command-line
+sudo apt-get install ros-noetic-ecl-streams
+sudo apt-get install ros-noetic-kobuki-msgs
+sudo apt-get install ros-noetic-kobuki-dock-drives
+sudo apt-get install ros-noetic-yocs-controllers
+sudo apt-get install ros-noetic-navigation
+# https://blog.csdn.net/asian_it_taleni/article/details/124698043
